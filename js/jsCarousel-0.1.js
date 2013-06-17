a4d49f20ce273b16b3c1b8c1c8b737bbfc4d77ba
Joost Vandorp <joostvandorp@gmail.com>
2013-06-16 23:16:11 -0400

    bug gix v0.02

--- js/jsCarousel-0.1.js
+++ js/jsCarousel-0.1.js
@@ -9,144 +9,138 @@
  */
 
 window.onload = function() {
+    "use strict";
+    if (window.jsCarousel === undefined) {
+        var jsCarousel = (function() {
+/*
+ *  Private Variables
+ */
 
-if ( typeof jsCarousel === "undefined" ){
-var jsCarousel = (function() {
+/** 
+ * -------------------APPLICATION SETTINGS-------------------------
+ * 
+ * Feel free to modify these settings but make sure 
+ * to give reasonable values. Do not input a string where the
+ * default value is a number.
+ * 
+ * */
 
-
-    /*
-     *  Private Variables
-     */
-
-    /** 
-     * -------------------APPLICATION SETTINGS-------------------------
-     * 
-     * Feel free to modify these settings but make sure 
-     * to give reasonable values. Do not input a string where the
-     * default value is a number.
-     * 
-     * */
-
-    var directionOfRotarion = 1;
-    var intervalTime = 3500;
+        var directionOfRotarion = 1;
+        var intervalTime = 3500;
     
-    /**
-     * -------------------APPLICATION INITIALIZERS --------------------
-     * 
-     * Do not make changes beyond this point. There are no presentation 
-     * values here, only program logic. If you wish to make changes to
-     * the way slides are displayed, please consult the ".css" file.
-     * 
-     * */
+/**
+ * -------------------APPLICATION INITIALIZERS --------------------
+ * 
+ * Do not make changes beyond this point. There are no presentation 
+ * values here, only program logic. If you wish to make changes to
+ * the way slides are displayed, please consult the ".css" file.
+ * 
+ * */
 
-    var slides = document.getElementsByClassName('jsCarousel-slide');
-    var ns = slides.length;
-    var displayQueue = {
+        var slides = document.getElementsByClassName('jsCarousel-slide');
+        var ns = slides.length;
+        var displayQueue = {
                prev:0,
                curr:0,
                next:0
-    };
+        };
 
-    /* 
-     * Private Functions
-     */
+/* 
+ * Private Functions
+ */
 
-    function hideSlides () {
-        slides[displayQueue.prev].style.display = "none";
-        slides[displayQueue.curr].style.display ="none";
-        slides[displayQueue.next].style.display = "none";
-        slides[displayQueue.prev].setAttribute("id", " ");
-        slides[displayQueue.curr].setAttribute("id", " ");
-        slides[displayQueue.next].setAttribute("id", " ");
-    }
-
-    function showSlides () {
-        slides[displayQueue.prev].setAttribute("id", "prev_slide");
-        slides[displayQueue.curr].setAttribute("id", 'curr_slide');
-        slides[displayQueue.next].setAttribute("id", 'next_slide');
-        slides[displayQueue.prev].style.display = "inline";
-        slides[displayQueue.curr].style.display ="inline";
-        slides[displayQueue.next].style.display = "inline";
-    }
-
-    function updateDisplayQueue (newcurrent) {
-        // when passed as string , it is a possition 
-        if( typeof newcurrent === "string"){
-            displayQueue.curr = parseInt(newcurrent,10);
+        function hideSlides () {
+            slides[displayQueue.prev].style.display = "none";
+            slides[displayQueue.curr].style.display ="none";
+            slides[displayQueue.next].style.display = "none";
+            slides[displayQueue.prev].setAttribute("id", " ");
+            slides[displayQueue.curr].setAttribute("id", " ");
+            slides[displayQueue.next].setAttribute("id", " ");
         }
-        //when passed as number, it is direction of rotarion
-        else if( typeof newcurrent === "number"){
-            if( newcurrent === 1){
-                displayQueue.curr = displayQueue.next;
-            } else{
-                displayQueue.curr = displayQueue.prev;
+
+        function showSlides () {
+            slides[displayQueue.prev].setAttribute("id", "prev_slide");
+            slides[displayQueue.curr].setAttribute("id", 'curr_slide');
+            slides[displayQueue.next].setAttribute("id", 'next_slide');
+            slides[displayQueue.prev].style.display = "inline";
+            slides[displayQueue.curr].style.display ="inline";
+            slides[displayQueue.next].style.display = "inline";
+       }
+
+        function updateDisplayQueue (newcurrent) {
+            // when passed as string , it is a possition 
+            if( typeof newcurrent === "string"){
+                displayQueue.curr = parseInt(newcurrent,10);
             }
-        }
-        else { 
-            if( directionOfRotarion === 1){
-                displayQueue.curr = displayQueue.next;
-            } else{
-                displayQueue.curr = displayQueue.prev;
+            //when passed as number, it is direction of rotarion
+            else if( typeof newcurrent === "number"){
+                if( newcurrent === 1){
+                    displayQueue.curr = displayQueue.next;
+                } else{
+                    displayQueue.curr = displayQueue.prev;
+                }
             }
+            else { 
+                if( directionOfRotarion === 1){
+                    displayQueue.curr = displayQueue.next;
+                } else{
+                        displayQueue.curr = displayQueue.prev;
+                }
+            }
+            displayQueue.prev = (displayQueue.curr - 1 + ns) % ns;
+            displayQueue.next = (displayQueue.curr + 1) % ns;
         }
-        displayQueue.prev = (displayQueue.curr - 1 + ns) % ns;
-        displayQueue.next = (displayQueue.curr + 1) % ns;
-    }
           
-    return {
+        return {
 
-        init: function () {
-            var i;
+            init: function () {
+                var i;
 
-            for (i = 0; i < ns; i=i+1) {
-                slides[i].style.display = "none";
-            }
-            updateDisplayQueue();
-            showSlides();
-        },
-
-        play : function () {
-
-            function playEvents() {
-                hideSlides();
+                for (i = 0; i < ns; i=i+1) {
+                    slides[i].style.display = "none";
+                }
                 updateDisplayQueue();
-                showSlides();}
+                showSlides();
+            },
 
-            var i, timeout, player = setInterval(playEvents, intervalTime);
+            play : function () {
 
-            document.body.onclick= function(evt){
-                evt=window.event ? evt.srcElement: evt.target;
+                function playEvents() {
+                    hideSlides();
+                    updateDisplayQueue();
+                    showSlides();}
 
-                if( evt.className.indexOf('jsCarousel-pslide')!=-1){
-                    clearInterval(player);
-                    hideSlides();
-                    updateDisplayQueue(evt.alt);
-                    showSlides();
-                    player = setInterval(playEvents, intervalTime);
-                }
-                else if( evt.id==="jsCarousel-next"){
-                   clearInterval(player);
-                    hideSlides();
-                    updateDisplayQueue(1);
-                    showSlides();
-                    player = setInterval(playEvents, intervalTime);
-                }
-                else if( evt.id==="jsCarousel-prev"){
-                    clearInterval(player);
-                    hideSlides();
-                    updateDisplayQueue(0);
-                    showSlides();
-                    player = setInterval(playEvents, intervalTime);
-                }
-                else{
-                    //do nothing
-                }
+                var player = setInterval(playEvents, intervalTime);
+
+                document.body.onclick= function(evt){
+                    evt = window.event ? evt.srcElement : evt.target;
+                    if( evt.className === "jsCarousel-pslide"){
+                        clearInterval(player);
+                        hideSlides();
+                        updateDisplayQueue(evt.alt);
+                        showSlides();
+                        player = setInterval(playEvents, intervalTime);
+                    }
+                    else if( evt.id==="jsCarousel-next"){
+                       clearInterval(player);
+                        hideSlides();
+                        updateDisplayQueue(1);
+                        showSlides();
+                        player = setInterval(playEvents, intervalTime);
+                    }
+                    else if( evt.id==="jsCarousel-prev"){
+                        clearInterval(player);
+                        hideSlides();
+                        updateDisplayQueue(0);
+                        showSlides();
+                        player = setInterval(playEvents, intervalTime);
+                    }
+                };
             }
-        }
-    };
-}());
-jsCarousel.init();
-jsCarousel.play();
-}
+        };
+    }());
+    jsCarousel.init();
+    jsCarousel.play();
+    }
 };
 
